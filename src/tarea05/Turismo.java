e75591b87aeacfa3ff2c2ed3d5e48ba566965a88
lol <lol@lol-X550LA>
2018-01-31 13:28:24 +0100

    class Turismo + (Constructor, constructor copia, método compruebaMatricula)

--- src/tarea05/Turismo.java
+++ src/tarea05/Turismo.java
@@ -5,6 +5,9 @@
  */
 package tarea05;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author lol
@@ -13,5 +16,36 @@ public class Turismo {
     
     private String matricula, marca, modelo;
     private int cilindrada;
+    private static int identificador;
+    
+    public Turismo(String matricula, String marca, String modelo, int cilindrada){
+                
+        this.marca=marca;
+        this.modelo=modelo;
+        if (cilindrada > 0 )
+            this.cilindrada=cilindrada;
+        else
+            throw new ExcepcionAlquilerVehiculos("No se admite esa cilindrada.");
+        if(compruebaMatricula(matricula))
+            this.matricula=matricula;
+        else
+            throw new ExcepcionAlquilerVehiculos("Matrícula incorrecta.");
+        identificador ++;
+        
+    }
+    
+    public Turismo(Turismo turismoCopia){
+        
+        marca=turismoCopia.getMarca();
+        modelo=turismoCopia.getModelo();
+        cilindrada=turismoCopia.getCilindrada();
+        matricula=turismoCopia.getMatricula();
+    }
+    
+    private boolean compruebaMatricula(String matricula){
+        Pattern regex = Pattern.compile("[0-9]{4}[a-z]{3}");
+        Matcher matcher = regex.matcher(matricula);
+        return matcher.matches();
+    }
     
 }
