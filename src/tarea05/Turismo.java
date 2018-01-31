1888b7a655554d529773bbf5e122cb9f9983656f
lol <lol@lol-X550LA>
2018-01-31 12:42:24 +0100

    class Turismo

---
+++ src/tarea05/Turismo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+/**
+ *
+ * @author lol
+ */
+public class Turismo {
+    
+    private String matricula, marca, modelo;
+    private int cilindrada;
+    
+}
