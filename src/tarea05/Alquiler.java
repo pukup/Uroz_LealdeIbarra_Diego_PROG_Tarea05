3bb386ae93ae209b12ad608df33c2925013f9ca3
lol <lol@lol-X550LA>
2018-01-31 18:27:03 +0100

    class Alquiler.

---
+++ src/tarea05/Alquiler.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author lol
+ */
+public class Alquiler {
+    
+    private Date fecha;
+    private int dias;
+    private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+    private final int MS_DIA = 1000*60*60*24;
+    private final double PRECIO_DIA;
+
+}
