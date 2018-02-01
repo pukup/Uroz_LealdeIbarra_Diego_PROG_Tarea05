11a8f3c2d3dbc6e6c29451416e8c01fd317d0815
lol <lol@lol-X550LA>
2018-02-01 10:33:28 +0100

    class AlquilerVehiculos

---
+++ src/tarea05/AlquilerVehiculos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+/**
+ *
+ * @author lol
+ */
+public class AlquilerVehiculos {
+    
+    private final int MAX_TURISMOS = 10;
+    private final int MAX_CLIENTES = 50;
+    private final int MAX_ALQUILERES = 10;
+    private Turismo[] turismos;
+    private Cliente[] clientes;
+    private Alquiler[] alquileres;
+    
+}
